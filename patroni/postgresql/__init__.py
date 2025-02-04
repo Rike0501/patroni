1fdade540c631e28bf60a52184694b7d4cf51a7e
unknown <Administrator@BULJA.LOCAL>
2025-02-04 09:42:16 +0100

    Added loop to iterate through every database chache and schedule rebuild

--- patroni/postgresql/__init__.py
+++ patroni/postgresql/__init__.py
@@ -1239,7 +1239,8 @@ class Postgresql(object):
             before_promote()
 
         self.slots_handler.on_promote()
-        self.mpp_handler.schedule_cache_rebuild()
+        for database in self._databases:
+            self.mpp_handler.schedule_cache_rebuild(database)
 
         ret = self.pg_ctl('promote', '-W')
         if ret:
